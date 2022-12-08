1c23dc5238573ee1ed7caaf7e23eddcdae82e6b7
caleb <caleb@repositpower.com>
2022-12-09 07:34:32 +1100

    Add solution to part 2 of day 8

--- day8/main.hs
+++ day8/main.hs
@@ -1,6 +1,6 @@
-import System.Environment
 import Data.Char (digitToInt)
-import Data.List (transpose)
+import Data.List (transpose, inits, tails, findIndex)
+import System.Environment
 
 isTreeVisibleL :: [Int] -> [Bool]
 isTreeVisibleL [] = []
@@ -19,11 +19,37 @@ isTreeVisibleR l = map snd . scanr isVisible (last l, True) $ init l
 isTreeVisible :: [Int] -> [Bool]
 isTreeVisible l = zipWith (||) (isTreeVisibleL l) (isTreeVisibleR l)
 
+solvePart1 :: [[Int]] -> Int
+solvePart1 rows = let
+    horizontal = map isTreeVisible rows
+    vertical = transpose . map isTreeVisible . transpose $ rows
+    visibleTrees = zipWith (||) (concat horizontal) (concat vertical)
+  in
+    sum . map fromEnum $ visibleTrees
+    
+    
+visibleFromTree :: Int -> [Int] -> Int
+visibleFromTree t [] = 0
+visibleFromTree t l = case findIndex (>= t) l of
+  Just i -> i + 1
+  Nothing -> length l
+
+visibleWithinRow :: [Int] -> [Int]
+visibleWithinRow l = zipWith3 innerFn l (init . inits $ l) (tail . tails $ l)
+  where innerFn t leftOf rightOf = visibleFromTree t (reverse leftOf) * visibleFromTree t rightOf
+
+solvePart2 :: [[Int]] -> Int
+solvePart2 rows = let
+    horizontal = map visibleWithinRow rows
+    vertical = transpose . map visibleWithinRow . transpose $ rows
+    visibleWithinForest = zipWith (*) (concat horizontal) (concat vertical)
+  in
+    maximum visibleWithinForest
+
+
 main = do
   inputPath <- head <$> getArgs
   input <- readFile inputPath
-  let rows = map (map digitToInt) . lines $ input :: [[Int]]
-  let horizontal = map isTreeVisible rows
-  let vertical = transpose . map isTreeVisible . transpose $ rows
-  let visibleTrees = zipWith (||) (concat horizontal) (concat vertical)
-  print . sum . map fromEnum $ visibleTrees
+  let rows = map (map digitToInt) . lines $ input
+  print . solvePart1 $ rows
+  print . solvePart2 $ rows
